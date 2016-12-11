1846698d7fa3aa4bf1f6b92610bea8adf6d8e40c
codeworkx <codeworkx@cyanogenmod.org>
2016-12-11 09:55:40 -0500

    s2: add twrp config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,6 +141,17 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := tr
 BOARD_HAS_NO_SELECT_BUTTON := true
 
+# Twrp
+RECOVERY_VARIANT := twrp
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_NTFS_3G := true
+
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 include device/qcom/sepolicy/sepolicy.mk
