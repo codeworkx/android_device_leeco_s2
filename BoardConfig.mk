19509ef048670376be37726391bf0fd0838d4d7a
codeworkx <codeworkx@cyanogenmod.org>
2016-12-11 09:57:21 -0500

    s2: fix board name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := MSM8976
+TARGET_BOOTLOADER_BOARD_NAME := MSM8952
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
