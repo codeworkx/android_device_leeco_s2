7b8f160c70c1489afbf1e88c2bdb22796b19843d
codeworkx <codeworkx@cyanogenmod.org>
2016-12-13 19:40:01 +0100

    s2: reorganize makefiles
    Change-Id: I0471fd359774b1ddc91ba9a3756a2a37e927c40f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,7 @@
 
 LOCAL_PATH := device/letv/s2
 
+# Headers
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Platform
--- cm.mk
+++ cm.mk
@@ -1,15 +1,12 @@
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
 # Inherit device configuration
 $(call inherit-product, device/letv/s2/full_s2.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
-
-# Release name
-PRODUCT_RELEASE_NAME := le_s2
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := s2
