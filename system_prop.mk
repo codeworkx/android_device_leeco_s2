10cf8addce4896839fe2146e50d52209bc946f2e
codeworkx <codeworkx@cyanogenmod.org>
2016-12-13 20:10:09 +0100

    s2: set frp
    Change-Id: Ic63c2c935d652ea31a0171a7a41c8d13b110b3c8

--- system_prop.mk
+++ system_prop.mk
@@ -6,3 +6,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 	rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 	rild.libargs=-d /dev/smd0
+
+# Factory Reset Protection
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/config
