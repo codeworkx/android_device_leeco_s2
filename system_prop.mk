821abca428d232edef5cf80910e8305aa3f36daf
codeworkx <daniel.hillenbrand@codeworkx.de>
2016-12-27 17:25:00 +0100

    s2: set default telephony props
    Change-Id: I6b29f3972c68ab10b6e3414d6e274f8e3a37cc51

--- system_prop.mk
+++ system_prop.mk
@@ -5,7 +5,10 @@
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
 	rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-	rild.libargs=-d /dev/smd0
+	rild.libargs=-d /dev/smd0 \
+	ro.telephony.default_network=9 \
+	telephony.lteOnCdmaDevice=0 \
+	telephony.lteOnGsmDevice=1
 
 # Factory Reset Protection
 PRODUCT_PROPERTY_OVERRIDES += \
