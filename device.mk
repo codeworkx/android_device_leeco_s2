7ac39ea5d0151edd6f3534514b49abfe57048a29
codeworkx <daniel.hillenbrand@codeworkx.de>
2016-12-27 16:56:37 +0100

    s2: fix usb config
    Change-Id: I5575075df75605289e70a3626628066db0a419ac

--- device.mk
+++ device.mk
@@ -231,6 +231,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/lowi.conf:system/etc/lowi.conf \
     $(LOCAL_PATH)/configs/xtwifi.conf:system/etc/xtwifi.conf
 
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-leeco
