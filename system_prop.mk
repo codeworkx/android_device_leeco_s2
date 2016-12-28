cd30a6fc64cf6b81ffdd85e405e7f1611f4c9b01
codeworkx <daniel.hillenbrand@codeworkx.de>
2016-12-28 15:49:50 +0100

    s2: add missing init file, set radio props
    Change-Id: I3eb1920715fa3bf1fa8727dd71e19fe21c1ecd33

--- system_prop.mk
+++ system_prop.mk
@@ -9,7 +9,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.telephony.default_network=9 \
 	telephony.lteOnCdmaDevice=0 \
 	telephony.lteOnGsmDevice=1 \
-	persist.radio.multisim.config=dsds
+	persist.radio.multisim.config=dsds \
+	persist.radio.calls.on.ims=0 \
+	persist.radio.jbims=0 \
+	persist.radio.csvt.enabled=false \
+	persist.radio.rat_on=combine \
+	persist.radio.mt_sms_ack=20
 
 # Factory Reset Protection
 PRODUCT_PROPERTY_OVERRIDES += \
